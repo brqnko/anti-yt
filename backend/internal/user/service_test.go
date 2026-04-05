14ec5e021c39034cabe1e3f11bc85effb7ada5a7
brqnko <161340972+brqnko@users.noreply.github.com>
2026-04-06 03:20:49 +0900

    TokenDurationFuncを追加

--- backend/internal/user/service_test.go
+++ backend/internal/user/service_test.go
@@ -64,6 +64,7 @@ func TestService_CreateNewUser(t *testing.T) {
 				SignUserAccessTokenFunc: func(_, _ uuid.UUID, _ string) (string, time.Time, error) {
 					return "access-token", time.Now().Add(time.Hour), nil
 				},
+				TokenDurationFunc: func() time.Duration { return 15 * time.Minute },
 			},
 			"http://localhost",
 		)
@@ -111,6 +112,7 @@ func TestService_CreateNewUser(t *testing.T) {
 				SignUserAccessTokenFunc: func(_, _ uuid.UUID, _ string) (string, time.Time, error) {
 					return "access-token", time.Now().Add(time.Hour), nil
 				},
+				TokenDurationFunc: func() time.Duration { return 15 * time.Minute },
 			},
 			"http://localhost",
 		)
