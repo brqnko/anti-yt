7c132e1d5720013d990eee1de0301ebae349e301
brqnko <161340972+brqnko@users.noreply.github.com>
2026-05-17 21:10:02 +0900

    change migration

--- backend/migrations/20260517204626_add_channel_last_seen_at.sql
+++ backend/migrations/20260517204626_add_channel_last_seen_at.sql
@@ -1,5 +1,6 @@
 -- +goose Up
 ALTER TABLE m_channel ADD COLUMN last_seen_at timestamptz NOT NULL DEFAULT current_timestamp;
+UPDATE m_channel SET last_seen_at = rss_fetched_at;
 ALTER TABLE m_channel ALTER COLUMN last_seen_at DROP DEFAULT;
 CREATE INDEX idx_3_m_channel ON m_channel(last_seen_at);
 
